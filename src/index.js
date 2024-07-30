597d29820de113ea1fd749039d0559261d1b28b8
feliperyo <ryofeliperyo@gmail.com>
2024-07-30 11:58:02 -0300

    configurando projeto

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+import Login from "./containers/Login";
+import GlobalStyles from "./styles/globalStyles";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<h1>Hello</h1>);
+root.render(
+    <>
+        <Login />
+        <GlobalStyles />
+    </>
+);
