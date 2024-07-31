1da9be61cc6b5ad7cf24fc525b4d1e625980f67a
feliperyo <ryofeliperyo@gmail.com>
2024-07-31 10:17:03 -0300

    tela de login

--- src/containers/Login/index.js
+++ src/containers/Login/index.js
@@ -1,10 +1,28 @@
 import React from "react";
 
+import LoginImg from '../../assets/burguer-login.svg'
+import Logo from '../../assets/logo.svg'
+
+import { Container, LoginImage, ContainerItems, Label, Input, Button, SignInLink } from './styles'
+
 function Login() {
   return (
-    <div>
-      <h1>Login</h1>
-    </div>
+    <Container>
+      <LoginImage src={LoginImg} alt="imagem hamburguer" />
+      <ContainerItems>
+        <img src={Logo} alt="logo" />
+        <h1>Login</h1>
+
+        <Label>Email</Label>
+        <Input />
+
+        <Label>Senha</Label>
+        <Input />
+
+        <Button>Entrar</Button>
+        <SignInLink>Não possui conta? <a>Cadastrar</a></SignInLink>
+      </ContainerItems>
+    </Container>
   );
 }
 
---
+++ src/containers/Login/styles.js
@@ -0,0 +1,100 @@
+import styled from "styled-components";
+import BackgroundImage from '../../assets/bg-login.jpg'
+
+export const Container = styled.div`
+height: 100vh;
+width: 100vw;
+background:url('${BackgroundImage}');
+background-size: cover;
+
+display: flex;
+justify-content: center;
+align-items: center;
+`
+
+export const LoginImage = styled.img`
+height: 70%;
+`
+
+export const ContainerItems = styled.div`
+border-radius:  0px 10px 10px 0px;
+background: #373737;
+box-shadow: 0px 4px 15px 0px #4A90E23D;
+height: 70%;
+padding: 25px 75px;
+
+display: flex;
+flex-direction: column;
+justify-content: center;
+
+h1{
+font-size: 24px;
+font-weight: 500;
+line-height: 28.13px;
+text-align: center;
+color: #fff;
+margin-top: 100px;
+}
+`
+
+export const Label = styled.p`
+font-size: 12px;
+font-weight: 500;
+line-height: 14.06px;
+color: #fff;
+margin: 28px 0 5px;
+`
+
+export const Input = styled.input`
+width: 391.42px;
+height: 38.32px;
+border-radius: 5px;
+box-shadow: 3px 3px 10px 0px #4A90E230;
+border: none;
+padding-left: 10px;
+`
+
+export const Button = styled.button`
+width: 182.81px;
+height: 36.13px;
+background: #9758A6;
+border: none;
+border-radius: 20px;
+cursor: pointer;
+margin: 75px auto 25px;
+
+font-size: 16px;
+font-weight: 500;
+line-height: 18.75px;
+text-align: center;
+color: #eee;
+
+&:hover{
+    opacity: .8;
+}
+
+&:active{
+    opacity: .6;
+}
+`
+
+export const SignInLink = styled.p`
+font-size: 14px;
+font-weight: 400;
+line-height: 16.41px;
+color: #fff;
+margin: 0 auto;
+
+a{
+    cursor: pointer;
+    text-decoration: underline;
+
+    &:hover{
+        opacity: .8;
+    }
+
+    &:active{
+    opacity: .6;
+    }
+}
+`
