ac491051149c7e8316e60ea987ae4a82798b067a
feliperyo <ryofeliperyo@gmail.com>
2024-08-09 11:29:46 -0300

    criando carrossel

--- src/components/CategoryCarousel/index.js
+++ src/components/CategoryCarousel/index.js
@@ -1,8 +1,10 @@
 import React, { useEffect, useState } from "react";
+import Carousel from 'react-elastic-carousel'
+
 
 import Category from '../../assets/category.png'
 import api from '../../services/api'
-import { Container, CategoryImg } from "./styles";
+import { Container, CategoryImg, ContainerItems, Image, Button } from "./styles";
 
 function CategoryCarousel() {
     const [categories, setCategories] = useState([])
@@ -17,9 +19,26 @@ function CategoryCarousel() {
         loadCategories()
     }, [])
 
+    const breakPoints = [
+        { width: 1, itemToShow: 1 },
+        { width: 400, itemToShow: 2 },
+        { width: 600, itemToShow: 3 },
+        { width: 900, itemToShow: 4 },
+        { width: 1300, itemToShow: 5 }
+    ]
+
     return (
         <Container>
             <CategoryImg src={Category} alt="logo da categoria" />
+
+            <Carousel itemsToShow={5} style={{ width: '90%' }} breakPoints={breakPoints}>
+                {categories && categories.map(category => (
+                    <ContainerItems key={category.id}>
+                        <Image src={category.url} alt="foto da categoria" />
+                        <Button>{category.name}</Button>
+                    </ContainerItems>
+                ))}
+            </Carousel>
         </Container>
     )
 }
--- src/components/CategoryCarousel/styles.js
+++ src/components/CategoryCarousel/styles.js
@@ -1,9 +1,27 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-
+    background-color: #EFEFEF;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 35px;
+    padding: 35px 0;
 `
 
 export const CategoryImg = styled.img`
+
+`
+
+export const ContainerItems = styled.div`
+display: flex;
+flex-direction: column;
+`
+
+export const Image = styled.img`
+    width: 200px;
+`
+
+export const Button = styled.button`
 
 `
