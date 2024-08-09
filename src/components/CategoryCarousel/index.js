c2e34d4e5f43b68b36db4020a58a67688e047639
feliperyo <ryofeliperyo@gmail.com>
2024-08-09 10:21:04 -0300

    ajustes

--- src/components/CategoryCarousel/index.js
+++ src/components/CategoryCarousel/index.js
@@ -1,10 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import Category from '../../assets/category.png'
-
+import api from '../../services/api'
 import { Container, CategoryImg } from "./styles";
 
 function CategoryCarousel() {
+    const [categories, setCategories] = useState([])
+
+    useEffect(() => {
+        async function loadCategories() {
+            const { data } = await api.get('categories')
+
+            setCategories(data)
+        }
+
+        loadCategories()
+    }, [])
+
     return (
         <Container>
             <CategoryImg src={Category} alt="logo da categoria" />
--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -1,23 +1,11 @@
-import React, { useEffect } from "react";
+import React from "react";
 
 import HomeLogo from '../../assets/home-logo.png'
 
 import { Container, HomeImg } from "./styles";
 import CategoryCarousel from '../../components/CategoryCarousel'
 
-import api from '../../services/api'
-
 function Home() {
-
-    useEffect(() => {
-        async function loadCategories() {
-            const response = await api.get('categories')
-
-            console.log(response)
-        }
-
-        loadCategories()
-    }, [])
 
     return (
         <Container>
