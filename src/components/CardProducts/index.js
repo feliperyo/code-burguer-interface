0cb31bd4041e707dd9786bfac314e2c14bc97339
feliperyo <ryofeliperyo@gmail.com>
2024-08-12 11:39:01 -0300

    criando lógica do carrinho

--- src/components/CardProducts/index.js
+++ src/components/CardProducts/index.js
@@ -1,9 +1,14 @@
 import React from "react";
 import PropTypes from 'prop-types'
+
+import { useCart } from "../../hooks/CartContext";
+
 import { Container, Image, ProductName, ProductPrice } from './styles'
 import { Button } from '../../components'
 
 export function CardProducts({ product }) {
+    const { putProductsInCart } = useCart()
+
     return (
         <Container>
             <Image src={product.url} alt="imagem do produto" />
@@ -11,7 +16,7 @@ export function CardProducts({ product }) {
             <div>
                 <ProductName>{product.name}</ProductName>
                 <ProductPrice>{product.formatedPrice}</ProductPrice>
-                <Button>Adicionar</Button>
+                <Button onClick={() => putProductsInCart(product)}>Adicionar</Button>
             </div>
 
         </Container>
