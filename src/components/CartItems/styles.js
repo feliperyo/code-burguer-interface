c8818a66a8eb5a1c54e219f0aafa1b92b691a232
feliperyo <ryofeliperyo@gmail.com>
2024-08-13 10:21:37 -0300

    adicionando e removendo itens do carrinho

--- src/components/CartItems/styles.js
+++ src/components/CartItems/styles.js
@@ -10,7 +10,7 @@ width: max-content;
 
 export const Header = styled.div`
 display: grid;
-grid-template-columns: repeat(5,1fr);
+grid-template-columns: repeat(6,1fr);
 padding: 10px;
 border-bottom: 1px solid #b5b5b5;
 
@@ -22,19 +22,51 @@ p{
 
 export const Body = styled.div`
 display: grid;
-grid-template-columns: repeat(5,1fr);
+grid-template-columns: repeat(6,1fr);
 width: max-content;
 grid-gap: 10px 15px;
 padding: 10px;
-
-img{
-    width: 120px;
-    height: 120px;
-    border-radius: 10px;
-}
 
 p{
     font-size: 16px;
     color: #000;
 }
+
+.quantity-container{
+    display: flex;
+    gap: 20px;
+
+    button{
+        height: 30px;
+        background: transparent;
+        border: none;
+        font-size: 24px;
+        cursor: pointer;
+        margin-top: -5px;
+    }
+}
+`
+
+export const ProductImage = styled.img`
+width: 120px;
+height: 120px;
+border-radius: 10px;
+`
+export const BtnTrash = styled.button`
+width: 20px;
+height: 20px;
+background: transparent;
+border: none;
+cursor: pointer;
+`
+
+export const TrashImage = styled.img`
+width: 20px;
+height: 20px;
+`
+
+export const EmptyCart = styled.p`
+padding: 20px;
+text-align: center;
+font-weight: bold;
 `
--- src/hooks/CartContext.js
+++ src/hooks/CartContext.js
@@ -27,6 +27,40 @@ export const CartProvider = ({ children }) => {
         await localStorage.setItem('codeburguer:cartInfo', JSON.stringify(newCartProducts))
     }
 
+    const deleteProducts = async productId => {
+        const newCart = cartProducts.filter(product => product.id !== productId)
+
+        setCartProducts(newCart)
+
+        await localStorage.setItem('codeburguer:cartInfo', JSON.stringify(newCart))
+    }
+
+    const increaseProducts = async productId => {
+        const newCart = cartProducts.map(product => {
+            return product.id === productId ? { ...product, quantity: product.quantity + 1 } : product
+        })
+
+        setCartProducts(newCart)
+
+        await localStorage.setItem('codeburguer:cartInfo', JSON.stringify(newCart))
+    }
+
+    const decreaseProducts = async productId => {
+        const cartIndex = cartProducts.findIndex(pd => pd.id === productId)
+
+        if (cartProducts[cartIndex].quantity > 1) {
+            const newCart = cartProducts.map(product => {
+                return product.id === productId ? { ...product, quantity: product.quantity - 1 } : product
+            })
+
+            setCartProducts(newCart)
+
+            await localStorage.setItem('codeburguer:cartInfo', JSON.stringify(newCart))
+        } else {
+            deleteProducts(productId)
+        }
+    }
+
     useEffect(() => {
         const loadCartProducts = async () => {
             const clientCartData = await localStorage.getItem('codeburguer:cartInfo')
@@ -40,7 +74,8 @@ export const CartProvider = ({ children }) => {
     }, [])
 
     return (
-        <CartContext.Provider value={{ putProductsInCart, cartProducts }}>
+        <CartContext.Provider
+            value={{ putProductsInCart, cartProducts, increaseProducts, decreaseProducts, deleteProducts }}>
             {children}
         </CartContext.Provider>
     )
