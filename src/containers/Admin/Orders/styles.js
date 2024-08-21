c1a1c008031b915bb5b0301f38d482217e8c834f
Felipe Ryo <ryofeliperyo@gmail.com>
2024-08-21 11:22:07 -0300

    criando tabela admin

---
+++ src/containers/Admin/Orders/styles.js
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+background-color: #EFEFEF;
+min-height: 100vh;
+padding: 20px;
+`
+
+export const ProductsImg = styled.img`
+width: 60px;
+border-radius: 5px;
+`
+
+export const InfoProducts = styled.div`
+display: flex;
+align-items: center;
+gap: 10px;
+`
