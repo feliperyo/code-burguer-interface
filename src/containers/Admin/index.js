b2e515895a262743c7b3861b098917f6d23b11ec
Felipe Ryo <ryofeliperyo@gmail.com>
2024-08-26 10:45:42 -0300

    estilizando tabela de produtos

--- src/containers/Admin/index.js
+++ src/containers/Admin/index.js
@@ -1,13 +1,17 @@
 import React from 'react'
 import Orders from './Orders'
-import { Container } from './styles'
+import { Container, ContainerItems } from './styles'
 import { SideMenuAdmin } from '../../components'
+import ListProducts from './ListProducts'
 
 export function Admin() {
     return (
         <Container>
             <SideMenuAdmin />
-            <Orders />
+            <ContainerItems>
+                {/* <Orders /> */}
+                <ListProducts />
+            </ContainerItems>
         </Container>
     )
 }
--- src/containers/Admin/styles.js
+++ src/containers/Admin/styles.js
@@ -2,8 +2,12 @@ import styled from "styled-components";
 
 export const Container = styled.div`
 display: flex;
-justify-content: space-between;
+justify-content: flex-start;
 min-height: 100vh;
 width: 100vw;
 background: #efefef;
+`
+
+export const ContainerItems = styled.div`
+padding: 20px;
 `
