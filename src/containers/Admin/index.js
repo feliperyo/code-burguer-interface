d097ac8e0ec77fae1c7fe356651c82d6949e4ebf
Felipe Ryo <ryofeliperyo@gmail.com>
2024-08-26 09:23:24 -0300

    criando menu admin

--- src/containers/Admin/index.js
+++ src/containers/Admin/index.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Orders from './Orders'
 import { Container } from './styles'
+import { SideMenuAdmin } from '../../components'
 
 export function Admin() {
     return (
         <Container>
+            <SideMenuAdmin />
             <Orders />
         </Container>
     )
